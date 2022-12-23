acaf6f749bd9b3a12d8ce08b263b53d3eb0bfc74
muthu ram <mr032495@gmail.com>
2022-12-23 22:25:14 +0530

    new

--- src/components/Reason/Reason.jsx
+++ src/components/Reason/Reason.jsx
@@ -4,18 +4,41 @@ import Image1 from '../../assets/image1.png'
 import Image2 from '../../assets/image2.png'
 import Image3 from '../../assets/image3.png'
 import Image4 from '../../assets/image4.png'
+import Tick from '../../assets/tick.png'
 const Reason = () => {
   return (
     <div className="Reason" id="Reason">
-        <div className='left-r'>
-            <img src={Image1} alt="" srcset="" />
-            <img src={Image2} alt="" srcset="" />
-            <img src={Image3} alt="" srcset="" />
-            <img src={Image4} alt="" srcset="" />
+      <div className='left-r'>
+        <img src={Image1} alt="" srcset="" />
+        <img src={Image2} alt="" srcset="" />
+        <img src={Image3} alt="" srcset="" />
+        <img src={Image4} alt="" srcset="" />
+      </div>
+      <div className='right-r'>
+        <span>Some Project</span>
+        <div>
+          <span className='stroke-text'>Ai </span>
+          <span>Project</span>
         </div>
-        <div className='right-r'>
-            <span></span>
+        <div className='details-r'>
+          <div>
+            <img src={Tick} alt=""></img>
+            <span>Reduced time for data-heavy tasks</span>
+          </div>
+          <div>
+            <img src={Tick} alt=""></img>
+            <span>Delivers consistent results</span>
+          </div>
+          <div>
+            <img src={Tick} alt=""></img>
+            <span>24x7 Availability</span>
+          </div>
+          <div>
+            <img src={Tick} alt=""></img>
+            <span>AI-powered virtual agents are always available</span>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
